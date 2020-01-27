a03fcb0d240c590e984a42247631b227d1bd32ed
Huda Fauzan Murtadho <hudafauzanm@gmail.com>
2020-01-27 17:04:49 +0700

    soal no 7 done

---
+++ soal7/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using McMaster.Extensions.CommandLineUtils;
+using System.Net;
+using System.Text.RegularExpressions;
+
+namespace soal7
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            var root = new CommandLineApplication()
+            {
+                Name = "#6 Get IP Address in private network",
+                Description = "Aplikasi Get IP Address in private network",
+                ShortVersionGetter = () => "1.0.0",
+            };
+
+            root.Command("ip-external",app => 
+            {
+                app.Description = "IP Address External String";
+
+                var text = app.Argument("Text","Masukkan Text");
+                app.OnExecute(() => 
+                {
+                    string externalIP = "";
+                    externalIP = (new WebClient()).DownloadString("http://checkip.dyndns.org/");
+                    externalIP = (new Regex(@"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}")).Matches(externalIP)[0].ToString();
+                    Console.WriteLine(externalIP);
+                });
+            });
+
+            return root.Execute(args);
+        }
+    }
+}

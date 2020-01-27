1415d5616a0072b6594acd3a8c5f7781f9e2dd6d
Huda Fauzan Murtadho <hudafauzanm@gmail.com>
2020-01-27 13:16:57 +0700

    Soal 3 done

---
+++ soal2/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using McMaster.Extensions.CommandLineUtils;
+
+
+namespace soal2
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            var root = new CommandLineApplication()
+            {
+                Name = "#2 Arithmetic",
+                Description = "Aplikasi Arithmetic",
+                ShortVersionGetter = () => "1.0.0",
+            };
+
+            root.Command("add",app => 
+            {
+                app.Description = "Menjumlahkan String";
+
+                var text = app.Argument("angka","Masukkan angka",true);
+                app.OnExecute(() => 
+                {
+                    int jumlah = 0;
+                    foreach(var x in text.Values)
+                    {
+                         jumlah += Convert.ToInt32(x) ;
+                    }
+                    Console.WriteLine(jumlah);
+                });
+            });
+
+            root.Command("subtract",app => 
+            {
+                app.Description = "Mengurangi String";
+
+                var text = app.Argument("angka","Masukkan angka",true);
+                app.OnExecute(() => 
+                {
+                    int jumlah = Convert.ToInt32(text.Values[0]);
+                    for(int i = 2;i <= text.Values.Count;i++)
+                    {
+                        jumlah -= Convert.ToInt32(text.Values[i-1]);
+                    } 
+                    Console.WriteLine(jumlah);
+                });
+            });
+            
+            root.Command("multiply",app => 
+            {
+                app.Description = "Mengalikan String";
+
+                var text = app.Argument("angka","Masukkan angka",true);
+                app.OnExecute(() => 
+                {
+                    int jumlah = 1;
+                    foreach(var x in text.Values)
+                    {
+                        jumlah = jumlah * Convert.ToInt32(x);
+                    }
+                    Console.WriteLine(jumlah);
+                });
+            });
+
+            root.Command("divide",app => 
+            {
+                app.Description = "Membagi String";
+
+                var text = app.Argument("angka","Masukkan angka",true);
+                app.OnExecute(() => 
+                {
+                    int jumlah = Convert.ToInt32(text.Values[0]);
+                    for(int i = 2;i <= text.Values.Count;i++)
+                    {
+                        jumlah = jumlah / Convert.ToInt32(text.Values[i-1]);
+                    } 
+                    Console.WriteLine(jumlah);
+                });
+            });
+
+             return root.Execute(args);
+        }
+    }
+}
---
+++ soal3/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using McMaster.Extensions.CommandLineUtils;
+
+namespace soal3
+{
+    class Program
+    {
+        public static bool palindrom (string text)
+        {
+            int length = text.Length;
+            for (int i = 0; i < length / 2; i++)
+            {
+                if (text[i] != text[length - i - 1])
+                {
+                return false;
+                }
+            }
+            return true; 
+        }
+        static int Main(string[] args)
+        {
+            
+            var root = new CommandLineApplication()
+            {
+                Name = "#3 Palindrome",
+                Description = "Aplikasi Palindrom",
+                ShortVersionGetter = () => "1.0.0",
+            };
+
+            root.Command("palindrom", app => 
+            {
+                app.Description = "Membagi String";
+
+                var text = app.Argument("angka","Masukkan angka");
+
+                app.OnExecute(() =>
+                {
+                    var hasil = text.Value.Replace(" ","").Replace(".","").Replace(",","").ToLower();
+                    if(palindrom(hasil) == true)
+                    {
+                        Console.WriteLine("String : " + text.Value);
+                        Console.WriteLine("Is Palindrom? Yes");
+                    }
+                    else{
+                        Console.WriteLine("String : " + text.Value);
+                        Console.WriteLine("Is Palindrom? No");
+                    }      
+                });
+            });
+
+            return root.Execute(args);
+        }
+    }
+}

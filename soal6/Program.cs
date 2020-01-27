0073a90984e34585273c1c3e3c98089211348e96
Huda Fauzan Murtadho <hudafauzanm@gmail.com>
2020-01-27 16:57:04 +0700

    soal 6 done

---
+++ soal6/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using McMaster.Extensions.CommandLineUtils;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+
+namespace soal6
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            var root = new CommandLineApplication()
+            {
+                Name = "#6 Get IP Address in private network",
+                Description = "Aplikasi Get IP Address in private network",
+                ShortVersionGetter = () => "1.0.0",
+            };
+
+            root.Command("ip",app => 
+            {
+                app.Description = "IP Address String";
+
+                var text = app.Argument("Text","Masukkan Text");
+                app.OnExecute(() => 
+                {
+                    var host = Dns.GetHostEntry(Dns.GetHostName());
+                    foreach (var ip in host.AddressList)
+                    {
+                        if (ip.AddressFamily == AddressFamily.InterNetwork)
+                        {
+                            Console.WriteLine(ip);
+                        }
+                    }
+                });
+            });
+
+            return root.Execute(args);
+        }
+    }
+}

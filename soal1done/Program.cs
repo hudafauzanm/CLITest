1f0a40baadcc04b74da35690e4d0c50186dbc2ff
Huda Fauzan Murtadho <hudafauzanm@gmail.com>
2020-01-28 15:22:32 +0700

    Soal 1

---
+++ soal1done/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace soal1done
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
